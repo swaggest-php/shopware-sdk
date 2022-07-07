5622261b2d989c253b71e72a5af98e0a4ebe2b74
Jakub Tapuć <jakub.groncki@gmail.com>
2022-07-07 22:33:56 +0200

    Fix tests

--- test/Struct/StructTest.php
+++ test/Struct/StructTest.php
@@ -22,8 +22,8 @@ final class StructTest extends TestCase
 
     public function provideApiAliases(): iterable
     {
-        yield ['bit_bag_shopware_sdk_test_struct_structs_test_struct', new TestStruct()];
-        yield ['bit_bag_shopware_sdk_test_struct_structs_another_test_struct', new AnotherTestStruct()];
-        yield ['bit_bag_shopware_sdk_test_struct_structs_yet_another_test_struct', new YetAnotherTestStruct()];
+        yield ['swaggest_shopware_sdk_test_struct_structs_test_struct', new TestStruct()];
+        yield ['swaggest_shopware_sdk_test_struct_structs_another_test_struct', new AnotherTestStruct()];
+        yield ['swaggest_shopware_sdk_test_struct_structs_yet_another_test_struct', new YetAnotherTestStruct()];
     }
 }
